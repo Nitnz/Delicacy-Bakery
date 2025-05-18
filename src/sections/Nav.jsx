8e3ce4de1b3bca86355138549e03084fc6dddbf1
Logesh nithin <logeshnithinzk@gmail.com>
2025-05-18 19:47:35 +0530

    Update Nav.jsx

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -7,26 +7,18 @@ const Nav = () => {
   <div className="relative flex items-center justify-between max-w-7xl mx-auto h-20">
 
     <div className="block md:hidden">
-      <button
-        className="text-2xl focus:outline-none">
+      <button className="text-2xl focus:outline-none">
         <i className='bx bx-menu'></i>
       </button>
     </div>
-
-    {/* Center Logo */}
     <div className="absolute left-1/2 transform -translate-x-1/2">
       <img src={Logo} alt="My Bakery Logo" className="h-24 md:h-28 lg:h-32 md:mt-5 object-contain" />
     </div>
-
-    {/* Desktop nav links */}
     <ul className="hidden md:flex items-center gap-6 lg:gap-10 lg:mr-24 font-semibold  font-nav text-base sm:text-lg lg:text-2xl ">
       {["ORDER", "PRODUCTS", "ABOUT US"].map((label, index) => (
         <li key={index} className="group relative cursor-pointer flex items-center gap-1">
           {label}
-          <img
-            src="https://img.icons8.com/?size=100&id=39786&format=png&color=000000"
-            className="w-3 h-3 transition-transform group-hover:rotate-180"
-          />
+          <img src="https://img.icons8.com/?size=100&id=39786&format=png&color=000000" className="w-3 h-3 transition-transform group-hover:rotate-180"/>
         </li>
       ))}
     </ul>

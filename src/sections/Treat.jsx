293b2b919629ad34c61e1893f6e1aa6157a75eb5
Logesh nithin <logeshnithinzk@gmail.com>
2025-05-18 19:52:11 +0530

    Update Treat.jsx

--- src/sections/Treat.jsx
+++ src/sections/Treat.jsx
@@ -4,7 +4,7 @@ const treat = () => {
   <main className="relative mt-20 w-full px-4 py-10">
   <div className="flex flex-col-reverse lg:flex-row items-center lg:items-start max-w-7xl mx-auto gap-8">
 
-    {/* Text Section */}
+    
     <div className="w-full lg:w-1/2 flex flex-col justify-center text-center lg:text-left px-4 sm:px-6">
       <h1 className="font-maglio font-extrabold text-4xl sm:text-5xl lg:text-6xl text-gray-800">
         Treats for any Occasion
@@ -19,14 +19,8 @@ const treat = () => {
         <span className="text-2xl">❞</span>
       </p>
     </div>
-
-    {/* Image Section */}
     <div className="w-full lg:w-1/2  lg:justify-end flex justify-center items-center">
-      <img 
-        src={Baking} 
-        alt="Freshly baked goods" 
-        className="max-w-full max-h-[400px] object-contain"
-      />
+      <img src={Baking} className="max-w-full max-h-[400px] object-contain" />
     </div>
 
   </div>
